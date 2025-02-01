349692bd675870209c2d8985ac9b24c2615b0ca5
MdRasel2000 <ctgmdraselislam@gmail.com>
2025-02-01 17:12:15 +0600

    js dataStructure Array

---
+++ script.js
@@ -0,0 +1,49 @@
+
+
+
+  //index arry
+// let marks = [ 87, 88, 80, 76, 59, 99, 56, 99]
+
+// console.log  (marks[0]);
+// console.log  (marks[1]);
+// console.log  (marks[2]);
+// console.log  (marks[3]);
+// console.log  (marks[4]);
+
+
+
+    //associative arry
+
+    // let human = {firstname :"abdul", lastname:"korim", age: 17, balance: 77}
+
+    //   console.log("firstname="+human.firstname);
+    //   console.log("lastname="+human.lastname);
+    //   console.log("age="+human.age);
+    //   console.log("balance="+human.balance);
+
+
+
+  let twoDimensional =
+  [
+
+    [22, 44, 43, 55, 66],
+    [88, 98, 87, 90, 76],
+    [67, 76, 89, 87, 43],
+    [76, 77, 99, 78, 75],
+
+  ]
+
+
+
+     console.log(twoDimensional[1][2]);
+     console.log(twoDimensional[2][3]);
+     console.log(twoDimensional[2][2]);
+
+
+
+       
+
+
+
+
+
